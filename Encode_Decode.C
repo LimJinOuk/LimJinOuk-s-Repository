da4b498c080b4539acc96737fc5450dc27d42d48
LimJinOuk <105255163+LimJinOuk@users.noreply.github.com>
2022-11-24 00:33:44 +0900

    Update Encode_Decode.C

--- Encode_Decode.C
+++ Encode_Decode.C
@@ -1,44 +1,78 @@
 #include<stdio.h>
 #include<string.h>
 
-const char* str;
-int length;
-char entered_PW[13];
-char encrypted_PW;
-char Decrypted_PW;
-const char* encodingTable = "wyhfxumtjvsgenbrdzlqapcoki";
-char* newStr;
 
-int checkCondiotion(const char* str, int length) {
-	if (length >= 8 && length <= 12) {
-		return 0;
-	}
-	else {
+int print_length(const char* str) {
+	int temp;
+	temp = strlen(str);
+}
+
+int checkCondition(const char* str, int length) {
+	if (length >= 9 && length <= 13) {
 		return 1;
 	}
+	else {
+		return 0;
+	}
 }
 
-void encode(const char* str, int length, const char* encodiongTable, char* newStr) {
+int check_Alphabet_Num(const char * str , int length) {
+	int checkdigit = 0;
+	int checkalpha = 0;
+	
+	for (int i = 0; i < length; i++) {
+		if (48 <= str[i] && str[i] <= 57) {
+			checkdigit = 3;
+		}
+
+		if (97 <= str[i] && str[i] <= 122) {
+			checkalpha = 1;
+		}
+
+		if (65 <= str[i] && str[i] <= 90) {
+			checkalpha = 1;
+		}
+	}
+	int result = checkalpha + checkdigit;
+	if (result > 3) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+void encode(const char* str, int length, const char* encodingTable, char* newStr) {
 
 }
 
-void decode(const char* str, int length, const char* encodiongTable, char* newStr) {
+void decode(const char* str, int length, const char* encodingTable, char* newStr) {
 
 }
 
 int main(void) {
-	int length = strlen(entered_PW);
+	int length;
+	const char* str[13];
+	char* encodingTable = "wyhfxumtjvsgenbrdzlqapcoki";
+	char* newStr;
+
 	while (1) {
 		printf("패스워드를 입력하세요(숫자와 알파벳을 혼합하여 8-12자):");
-		fgets(entered_PW, sizeof(entered_PW), stdin);
-		if (checkCondiotion(str, length) == 1) {
-			printf("%s\n", entered_PW);
-			printf("%d\n", length);
-			printf("범위 내의 문자 수로 다시 입력하세요.:\n");
+		fgets(str, sizeof(str), stdin);
+		length = print_length(str);
+		
+		if (checkCondition(str, length) == 1) {
+			if (check_Alphabet_Num(str , length) == 1) {
+				printf("입력하신 패스워드가 로 암호화 되었습니다.\n");
+				break;
+			}
+			else if (check_Alphabet_Num(str , length) == 0) {
+				printf("숫자와 알파벳을 혼합해 주세요.\n");
+			}
+			
 		}
 		else {
-			break;
+			printf("범위 내의 문자 수로 다시 입력하세요:\n");
 		}
 	}
-	return 0;
 }
